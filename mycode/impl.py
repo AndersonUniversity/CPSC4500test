10f0e02d6730eef61173d52a8ff9c4bca1f54b4f
Kyle M. Tarplee <kmtarplee@anderson.edu>
2015-08-21 06:58:17 -0400

    first cut at the Travis CI hooks

---
+++ mycode/impl.py
@@ -0,0 +1,8 @@
+import math
+import tornado
+
+def f(x, y, z):
+	return x + y + x
+
+def h(x):
+	return math.sin(x)
---
+++ mycode/impl_test.py
@@ -0,0 +1,7 @@
+
+def test_f():
+	assert f(1, 2, 3) == 6
+	assert f(2, 4, 6) == 12
+
+def test_h():
+	assert h(0) == 45.34
