ff86f035be35c8f25ea6e00bb70a6c2e39303c85
Kyle M. Tarplee <kmtarplee@anderson.edu>
2015-08-22 07:01:07 -0400

    fixed the broken test

--- mycode/impl_test.py
+++ mycode/impl_test.py
@@ -5,5 +5,5 @@ def test_f():
 	assert impl.f(2, 4, 6) == 12
 
 def test_h():
-	assert impl.h(0) == 45.34, "this should fail"
+	#assert impl.h(0) == 45.34, "this should fail"
 	assert impl.h(0) == 0.0, "this should pass"
