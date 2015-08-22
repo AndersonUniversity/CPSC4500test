f348c929cf827595fe6fb0a876688fc7b65db2fe
Kyle M. Tarplee <kmtarplee@anderson.edu>
2015-08-22 06:57:55 -0400

    refining my unit test so it might actually work

--- mycode/impl_test.py
+++ mycode/impl_test.py
@@ -1,8 +1,9 @@
+import impl
 
 def test_f():
-	assert f(1, 2, 3) == 6
-	assert f(2, 4, 6) == 12
+	assert impl.f(1, 2, 3) == 6
+	assert impl.f(2, 4, 6) == 12
 
 def test_h():
-	assert h(0) == 45.34, "this should fail"
-	assert h(0) == 0.0, "this should pass"
+	assert impl.h(0) == 45.34, "this should fail"
+	assert impl.h(0) == 0.0, "this should pass"
