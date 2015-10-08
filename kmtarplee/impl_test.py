7150123617e9dced54c1e93f7b9e658b44d1cbd7
Kyle M. Tarplee <kmtarplee@anderson.edu>
2015-10-08 11:08:03 -0400

    added flake8 testing

--- kmtarplee/impl_test.py
+++ kmtarplee/impl_test.py
@@ -1,9 +1,12 @@
 from . import impl
 
+
 def test_f():
-	assert impl.f(1, 2, 3) == 6
-	assert impl.f(2, 4, 6) == 12
+    assert impl.f(1, 2, 3) == 6
+    assert impl.f(2, 4, 6) == 12
+
 
 def test_h():
-	#assert impl.h(0) == 45.34, "this should fail"
-	assert impl.h(0) == 0.0, "this should pass"
+
+    # assert impl.h(0) == 45.34, "this should fail"
+    assert impl.h(0) == 0.0, "this should pass"
