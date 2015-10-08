b48cc14abe199cc983f5a532fc85f68607c24ab7
AndersonTylerJackson <tjackson2@anderson.edu>
2015-10-08 13:52:47 -0400

    Committing lab4!

---
+++ tjackson2/lab4.py
@@ -0,0 +1,22 @@
+'''09/28/2015 Tyler Jackson  Functions that handle testing for magic numbers. Includes a function to test to see if the number
+    is in magic number format, meaning decreasing, and a function to reverse a number.'''
+def decreasing_or_not(pInt):
+    int_array = list(map(int, str(pInt)))
+    for i in range(0, len(int_array)-1):
+        if int_array[i] < int_array[i+1]:
+            return False
+    return True
+
+def reverse_number(pInt):
+    int_string = str(pInt)
+    int_reversed = int(int_string[::-1])
+    return int_reversed
+
+def magic_or_not(pInt):
+    if pInt > 99 and pInt < 1000:
+        if decreasing_or_not(pInt) == True:
+            step_one = pInt - reverse_number(pInt)
+            step_two = step_one + reverse_number(step_one)
+            return step_two
+        return 'Not a decreasing number.'
+    return 'Number needs to be between 100 and 999.'
---
+++ tjackson2/lab4_test.py
@@ -0,0 +1,9 @@
+'''09/28/2015 Tyler Jackson Testing Implementation for magic tests. Range of all whole numbers from 100 through 999.
+    Not working as intended, throws an AssertionError that I can't seem to figure out. Works if asserted with just one number.'''
+import lab4
+
+def magic_or_not_test():
+    magic_counter = 0
+    for i in range(100, 1000):
+        assert lab4.magic_or_not(i) == 1089
+
