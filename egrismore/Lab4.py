912cca9236c37bd6ca1e0636e57ee769661a43ac
Emily Grismore <emilygrismore@Emily-Grismores-MacBook-Pro.local>
2015-10-08 13:48:30 -0400

    This is my magical 1089 code!

---
+++ egrismore/Lab4.py
@@ -0,0 +1,29 @@
+__author__ = 'emilygrismore'
+"""checks requirements for and runs the magic 1089"""
+
+
+def flip(i):
+    """reverses the number"""
+    int(str(i)[::-1])
+    return i
+
+
+def decrease(i):
+    """checks to see if the #'s are in descending order"""
+    largest = 10
+    for new in str(i):
+        number = int(new)
+        if number >= largest:
+            return False
+        else:
+            largest = number
+    return True
+
+
+def trick():
+    """shows the magical 1089"""
+    for i in range(100, 999):
+        if decrease(i):
+            difference = i - flip(i)
+            final = difference + flip(difference)
+            print(i)
---
+++ egrismore/Test_Lab4.py
@@ -0,0 +1,6 @@
+__author__ = 'emilygrismore'
+import Lab4
+
+
+def test_lab4():
+    Lab4.trick()
