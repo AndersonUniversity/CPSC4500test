87f08c995684ed4b331cecf2c32ad6eeaeb79618
Neal Tardy <ndtardy@anderson.edu>
2015-10-09 13:36:03 -0400

    removed whitespace at end of file and unused variable

--- ndtardy/testLab4.py
+++ ndtardy/testLab4.py
@@ -8,8 +8,6 @@ testLab4 - file to test the functions in lab4
 
 
 def test_lab4_functions():
-    count = 0
     for i in range(100, 1000):
         if lab4.is_decreasing(i):
             assert lab4.calc(i) == 1089
-
