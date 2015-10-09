40e636f93f6b7b2e3bf2af99452b2931c40cad1c
Elliot Womack <elliot.womack@e-gineering.com>
2015-10-09 13:36:46 -0400

    ran flake8 and fixed two files.

--- ewomack/test_lab4.py
+++ ewomack/test_lab4.py
@@ -1,11 +1,13 @@
 __author__ = 'elliotwomack'
 
+
 from . import lab4
 
+
 def test_magic1089():
-    assert lab4.magic1089(532)==1089
+    assert lab4.magic1089(532) == 1089
+
 
 def test_magic1089_failed():
     for i in range(100-899):
-        assert lab4.magic1089(i)=="You didnt enter an increasing number." or lab4.magic1089(i)==1089
-
+        assert lab4.magic1089(i) == "Wrong" or lab4.magic1089(i) == 1089
