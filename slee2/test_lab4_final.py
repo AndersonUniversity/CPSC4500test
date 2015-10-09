7273168a784ca0fdb7441a87ab48331197ded617
Samuel Lee <sLee@Samuels-MacBook.local>
2015-10-09 13:26:45 -0400

    This is magic number final has two functions. test_lab4_final.py has nose test.

---
+++ slee2/test_lab4_final.py
@@ -0,0 +1,12 @@
+__author__ = 'sLee'
+''' In unit test (use nosetests), use range() to generate a list that contains all numbers from 100 to 999.'''
+import lab4_final
+
+
+def test_generate_num():
+    """ unit test that generate nums and execute functions. """
+    for x in range(100, 1000):
+        if lab4_final.check_valid(x):
+            finalnum = lab4_final.magic_n(x)
+            print(finalnum)
+            assert finalnum == 1089
