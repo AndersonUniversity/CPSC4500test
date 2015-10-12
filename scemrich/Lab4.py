e07d9f503475e42335bd904e060c41045abf7fd8
scemrich <semrich@anderson.edu>
2015-10-11 21:36:11 -0400

    commit 9:36

--- scemrich/Lab4.py
+++ scemrich/Lab4.py
@@ -1,6 +1,5 @@
 __author__ = 'Connor  Emrich'
 
-
 '''Write down a three-digit number whose digits are decreasing.
    Then reverse the digits to create a new number, and subtract
    is number from the original number. With the resulting number,
@@ -10,7 +9,7 @@ __author__ = 'Connor  Emrich'
 def check_decreasenum(number1):
     numberstring = str(number1)
     if numberstring[0] > numberstring[1] > numberstring[2] \
-            and len(numberstring) == 3:
+            and 3 == len(numberstring):
         return True
     else:
         return False
@@ -26,7 +25,6 @@ def do_the_math(number1):
 
 
 def flip_number(number1):
-    flippednum = ''
     flippednum = str(number1)
     stringflippednum = str(flippednum)
     stringflippednum[::-1]
