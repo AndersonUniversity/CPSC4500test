7b2eb487ebea7019cabf20ec4f13747fb9a911c2
Cameron Gamble <csgamble@anderson.edu>
2015-10-12 13:48:23 -0400

    Changes made

--- csgamble/test.py
+++ csgamble/test.py
@@ -5,11 +5,9 @@ __author__ = 'Cam-Mac'
 from . import Magic
 
 
-# creates a list of numbers 100-999
-container = range(100, 999)
-
-
 # runs the test
 def test_yes():
+    # creates a list of numbers 100-999
+    container = range(100, 999)
     for i in container:
         Magic.calculate(i)
