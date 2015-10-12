ceb2a535b54b9b1ad205c4cdca08b99fa96cb1db
Elliot Womack <elliot.womack@e-gineering.com>
2015-10-12 13:53:26 -0400

    I included is decreasing in the test file and changed the test to where it always returned the number. I also deleted some pyc files.

--- ewomack/lab4.py
+++ ewomack/lab4.py
@@ -2,12 +2,6 @@ __author__ = 'elliotwomack'
 """ Test the magic 1089 """
 
 
-def is_decreasing(foo):
-    if(foo[0] > foo[1] > foo[2]):
-        return True
-    return False
-
-
 def reverse(foo):
     """reverse a number"""
     numSplit = list(str(foo))
@@ -16,7 +10,5 @@ def reverse(foo):
 
 def magic1089(foo):
     """test magic 1089"""
-    if(is_decreasing(list(str(foo)))):
-        num = foo - int(reverse(foo))
-        return num + int(reverse(num))
-    return "You didnt enter an increasing number."
+    num = foo - int(reverse(foo))
+    return num + int(reverse(num))
--- ewomack/test_lab4.py
+++ ewomack/test_lab4.py
@@ -4,10 +4,17 @@ __author__ = 'elliotwomack'
 from . import lab4
 
 
+def is_decreasing(foo):
+    if(foo[0] > foo[1] > foo[2]):
+        return True
+    return False
+
+
 def test_magic1089():
     assert lab4.magic1089(532) == 1089
 
 
 def test_magic1089_failed():
     for i in range(100-899):
-        assert lab4.magic1089(i) == "Wrong" or lab4.magic1089(i) == 1089
+        if is_decreasing(i):
+            assert lab4.magic1089(i) == 1089
