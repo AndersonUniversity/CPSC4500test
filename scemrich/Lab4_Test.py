c7b47594831c7cc6fcb8da20d9146ef64bc67ac3
scemrich <semrich@anderson.edu>
2015-10-12 12:13:16 -0400

    Commit last fix

--- scemrich/Lab4_Test.py
+++ scemrich/Lab4_Test.py
@@ -4,6 +4,6 @@ from . import Lab4
 def test_Lab4():
     '''nose test that cycles through all of the three digit numbers'''
     for i in range(100, 999):
-       if Lab4.check_decreasenum(i):
-           Lab4.do_the_math(i)
+        if Lab4.check_decreasenum(i):
+            Lab4.do_the_math(i)
     print(i)
