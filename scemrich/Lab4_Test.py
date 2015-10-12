7049d3073770722a5b59f8e68504d9d3f179cd2b
scemrich <semrich@anderson.edu>
2015-10-12 12:11:39 -0400

    commit monday 12:11pm

--- scemrich/Lab4_Test.py
+++ scemrich/Lab4_Test.py
@@ -1,12 +1,9 @@
-__author__ = 'Connor  Emrich'
-
-from  . import Lab4
+from . import Lab4
 
 
 def test_Lab4():
-    for i in range(100,999):
-       if Lab4.check_decreasenum(i) == True:
+    '''nose test that cycles through all of the three digit numbers'''
+    for i in range(100, 999):
+       if Lab4.check_decreasenum(i):
            Lab4.do_the_math(i)
     print(i)
-
-#nose test that cycles through all of the three digit numbers
