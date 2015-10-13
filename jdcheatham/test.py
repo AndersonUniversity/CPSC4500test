4c478eb1449b2dde47cb48824d2231433fc8b020
jdcheatham <jdcheatham@anderson.edu>
2015-10-12 20:28:18 -0400

    Update test.py
    Made recommended changes here too.

--- jdcheatham/test.py
+++ jdcheatham/test.py
@@ -1,14 +1,10 @@
 """J. Cheatham
 CPSC4500, Lab4Test'"""
 
+
 import Lab4
 
 
 def test_magic():
-    for number in range(100,999):
+    for number in range(100, 999):
         assert Lab4.magic(number) == 1098
-
-
-
-
-
