040639a72e9cd74db9b64c1b538f0271d0158a40
Ivaan Shrestha <ishrestha@anderson.edu>
2015-10-12 22:05:22 -0400

    fixed white space and inline comments

--- ishrestha/lab4Test.py
+++ ishrestha/lab4Test.py
@@ -4,26 +4,31 @@ in lab 4 is wokring as it supposed to
 Date: 09/27/2015
 Prof. Coy and Prof. Tarplee'''
 
-from lab4 import numCheck #imports numCheck function from lab4
-from lab4 import numConvert #imports numConvert function from lab 4
+from lab4 import numCheck  # imports numCheck function from lab4
+from lab4 import numConvert  # imports numConvert function from lab 4
+
 
 def test_numCheck():
+
     '''to test numCheck'''
-    for i in range(100,999):
-        assert numCheck(i)== True
+    for i in range(100, 999):
+        assert numCheck(i) == True
+
+
 def test_numConvert():
+
     '''to test numConvert'''
-    count =0
+    count = 0
     for i in range(100,999):
         if numConvert(i) == 1089:
-            count=count +1
+            count = count + 1
         else:
-            count =count
+            count = count
 
     return count
 
 if __name__ == '__main__':
-    print(test_numConvert()) #prints number of results with 1089
+    print(test_numConvert())  # prints number of results with 1089
 
 
 
