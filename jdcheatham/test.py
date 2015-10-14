2b96334609ca993a1b8d325d08427085cec9cf1f
jdcheatham <jdcheatham@anderson.edu>
2015-10-13 20:32:52 -0400

    Update test.py
    W293 blank line contains whitespace
    
    W391 blank line at end of file

--- jdcheatham/test.py
+++ jdcheatham/test.py
@@ -1,5 +1,5 @@
 """J. Cheatham
-CPSC4500, Lab4Test'"""
+CPSC4500, Lab4Test"""
 
 
 import Lab4
@@ -9,4 +9,3 @@ import Lab4
 def test_magic():
     for number in range(100, 999):
         assert Lab4.magic(number) == 1098
-        
